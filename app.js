d1773ab341653fcf89c1b55c0f40ad94d4914c15
marlondametto <marlondametto@gmail.com>
2018-02-23 16:07:16 -0300

    Chat com socket.io

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var express = require('express')
 	, methodOverride = require('method-override')
 	, error = require('./middlewares/error')
 	, app = express()
+	, server = require('http').Server(app)
+	, io = require('socket.io')(server)
 ;
 
 app.set('views', __dirname + '/views');
@@ -19,10 +21,19 @@ app.use(express.static(__dirname + '/public'));
 
 load('models').then('controllers').then('routes').into(app);
 
+io.sockets.on('connection', function(client)
+{
+	client.on('send-server', function(data){
+		var msg = "<b>"+data.nome+":</b>"+data.msg+"<br>";
+		client.emit('send-client', msg);
+		client.broadcast.emit('send-client', msg);
+	});
+});
+
 // middlewares de tratamento de erros
 app.use(error.notFound);
 app.use(error.serverError);
 
-app.listen(3000, function(){
+server.listen(3000, function(){
 	console.log('Ntalk no ar');
 });
---
+++ routes/chat.js
@@ -0,0 +1,7 @@
+// routes/chat.js
+
+module.exports = function(app) {
+    var autenticar = require('./../middlewares/autenticador')
+        , chat = app.controllers.chat;
+    app.get('/chat', autenticar, chat.index);
+};
