9ce3cc5a356380f25c867ec60dc81eef53872896
marlondametto <marlondametto@gmail.com>
2018-03-06 21:51:21 -0300

    6.5 Socket.io e Express usando a mesma sessão pág 76

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//import { Socket } from 'dgram';
+const KEY = 'ntalk.sid', SECRET = 'ntalk';
 
 var express = require('express')
 	, load = require('express-load')
@@ -10,16 +10,40 @@ var express = require('express')
 	, app = express()
 	, server = require('http').Server(app)
 	, io = require('socket.io')(server)
+	, cookie = cookieParser(SECRET)
+	, store = new expressSession.MemoryStore()
 ;
 
+// middlewares
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(cookieParser('ntalk'));
-app.use(expressSession());
+app.set(cookie);
+app.use(expressSession({
+	secret: SECRET,
+	name: KEY,
+	resave: true,
+	saveUninitialized: true,
+	store: store
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
+
+io.use(function(socket, next){
+	var data = socket.request;
+	cookie(data, {}, function(err){
+		var sessionID = data.signedCookies[KEY];
+		store.get(sessionID, function(err, session){
+			if(err || !session){
+				return next(new Error('acesso negado'));
+			} else {
+				socket.handshake.session = session;
+				return next();
+			}
+		});
+	});
+});
 
 load('models').then('controllers').then('routes').into(app);
 load('sockets').into(io);
--- controllers/chat.js
+++ controllers/chat.js
@@ -3,8 +3,7 @@
 module.exports = function(app) {
     var ChatController = {
         index: function(req, res){
-            var params = {usuario: req.session.usuario};
-            res.render('chat/index', params);
+            res.render('chat/index');
         }
     };
     return ChatController;
